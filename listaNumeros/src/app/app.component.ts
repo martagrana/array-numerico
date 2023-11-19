fd4e82fc90c88a8b2f46c0a981805108a9c25582
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-19 20:44:16 +0100

    funcion y boton sumatorio listo

--- listaNumeros/src/app/app.component.ts
+++ listaNumeros/src/app/app.component.ts
@@ -8,10 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'listaNumeros';
 
-  numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8,]
+  numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8,];
 
-  sumatorio() {
+  resultadoSumatorio: number = 0;
 
+  calcularSumatorio() {
+    for (let index = 0; index < this.numeros.length; index++) {
+      const element = this.numeros[index];
+      this.resultadoSumatorio += this.numeros[index];
+    }
+    return this.resultadoSumatorio;
   }
   maximo() {
 
@@ -19,5 +25,7 @@ export class AppComponent {
   minimo() {
 
   }
+
+
 
 }
