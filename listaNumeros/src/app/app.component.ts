dedf59b1af7cc3af373af0efd0e5154527c519e2
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-19 17:15:05 +0100

    pensar html y funciones necesarias, crear array

---
+++ listaNumeros/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'listaNumeros';
+
+  numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8,]
+
+  sumatorio() {
+
+  }
+  maximo() {
+
+  }
+  minimo() {
+
+  }
+
+}
