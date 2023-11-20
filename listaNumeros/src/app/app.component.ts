2ff5cd2f57021df061793718327fe718abb9ec85
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-20 11:56:05 +0100

    input para añadir numeros y resultados listos

--- listaNumeros/src/app/app.component.ts
+++ listaNumeros/src/app/app.component.ts
@@ -9,14 +9,21 @@ export class AppComponent {
   title = 'listaNumeros';
 
   numeros: number[] = [];
+  nuevoNumero: number = 0;
+
 
   resultadoSumatorio: number = 0;
   resultadoMaximo: number = 0;
   resultadoMinimo: number = 0;
 
+  agregarNumero() {
+    this.numeros.push(this.nuevoNumero);
+    this.nuevoNumero = 0;
+  }
+
   calcularSumatorio() {
     for (let index = 0; index < this.numeros.length; index++) {
-      const element = this.numeros[index];
+      /*const element = this.numeros[index];*/
       this.resultadoSumatorio += this.numeros[index];
     }
     return this.resultadoSumatorio;
