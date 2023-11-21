7aa10e559bd9204ff0b1264e50acda272c3b7500
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-21 11:42:38 +0100

    2 versiones. introducir numeros  introducir tamaño

--- listaNumeros/src/app/app.component.ts
+++ listaNumeros/src/app/app.component.ts
@@ -8,34 +8,68 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'listaNumeros';
 
+  /*propiedades version 1 */
   numeros: number[] = [];
   nuevoNumero: number = 0;
+  resultadoSumatoriov1: number = 0;
+  resultadoMaximov1: number = 0;
+  resultadoMinimov1: number = 0;
 
+  /*propiedades version 2 */
+  tamanoMuestra: number = 0;
+  muestraGenerada: number[] = [];
+  resultadoSumatoriov2: number = 0;
+  resultadoMaximov2: number = 0;
+  resultadoMinimov2: number = 0;
 
-  resultadoSumatorio: number = 0;
-  resultadoMaximo: number = 0;
-  resultadoMinimo: number = 0;
+  /*funciones verion 1 */
 
   agregarNumero() {
     this.numeros.push(this.nuevoNumero);
     this.nuevoNumero = 0;
   }
-
-  calcularSumatorio() {
+  calcularSumatoriov1() {
     let sumatorio: number = 0;
     for (let index = 0; index < this.numeros.length; index++) {
 
       sumatorio += this.numeros[index];
     }
-    this.resultadoSumatorio = sumatorio;
+    this.resultadoSumatoriov1 = sumatorio;
   }
-  encontrarMaximo() {
-    this.resultadoMaximo = Math.max(...this.numeros);
-    return this.resultadoMaximo;
+  encontrarMaximov1() {
+    this.resultadoMaximov1 = Math.max(...this.numeros);
+    return this.resultadoMaximov1;
   }
-  encontrarMinimo() {
-    this.resultadoMinimo = Math.min(...this.numeros);
-    return this.resultadoMinimo;
+  encontrarMinimov1() {
+    this.resultadoMinimov1 = Math.min(...this.numeros);
+    return this.resultadoMinimov1;
   }
 
+  /*funciones verion 2 */
+
+  generarMuestra(): number[] {
+    let numeroGenerado: number = 0;
+    for (let i = 0; i < this.tamanoMuestra; i++) {
+      numeroGenerado = Math.random();
+
+      this.muestraGenerada.push(numeroGenerado);
+    }
+    return this.muestraGenerada;
+  }
+  calcularSumatoriov2() {
+    let sumatorio: number = 0;
+    for (let index = 0; index < this.numeros.length; index++) {
+
+      sumatorio += this.muestraGenerada[index];
+    }
+    this.resultadoSumatoriov2 = sumatorio;
+  }
+  encontrarMaximov2() {
+    this.resultadoMaximov2 = Math.max(...this.muestraGenerada);
+    return this.resultadoMaximov2;
+  }
+  encontrarMinimov2() {
+    this.resultadoMinimov2 = Math.min(...this.muestraGenerada);
+    return this.resultadoMinimov2;
+  }
 }
